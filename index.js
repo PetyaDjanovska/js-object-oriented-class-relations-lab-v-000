43aa0c56ef28f69692fc1ce3171f9c73a6a916fd
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-26 15:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let store = { drivers: [];
+let store = { drivers: [] };
 let driverId = 0;
 
 class Driver {
   constructor(name) {
     this.id = ++driverId;
     this.name = name;
+    store.drivers.push(this);
   }
 }
